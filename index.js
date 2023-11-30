d5e9ebc9624a21e1d1db276131be28973140bf2f
Jeremy-Nourri <nourri.jer@gmail.com>
2023-11-30 19:13:08 +0100

    refactor: delete tailwind and use css

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-/*
-papier2 > pierre1
-pierre1 > ciseaux0
-ciseaux0 > papier2
-
-let playerComputer1
-let playerComputer2
-let playerUser
-
-playerUser peut sélectionner soit Pierre ou Feuille ou Ciseaux
-
-Un tableau regroupant Pierre ou Feuille ou Ciseaux.
-Une fonction qui génére un nombre aléatoire entre 0 et 2 qui correspondra à l'index des éléments du tableau.
-
-
-
-*/
 "use strict"
 
 const choiceArray = ["pierre", "feuille", "ciseaux"]
@@ -158,6 +141,10 @@ const compareChoice = (userChoice) => {
     }
 }
 
-user.choice = prompt("nombre")
+function saveNames() {
+    user.name = document.getElementById("nameUser").value;
+}
+
+// user.choice = prompt("nombre")
 
 compareChoice(user.choice)
