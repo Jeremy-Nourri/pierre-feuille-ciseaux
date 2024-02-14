8b247871695332dcc0f0d2db93a4502a6a8353ce
Jeremy-Nourri <nourri.jer@gmail.com>
2024-02-14 13:44:13 +0100

    refactor: index.js

--- choice/choice.js
+++ choice/choice.js
@@ -1,14 +1,2 @@
-function getUserName() {
-
-    const nickname = localStorage.getItem("userName");
-    const pUserName = document.querySelector(".container-choice-user__name");
-    console.log(pUserName);
-    console.log(nickname);
-    if (nickname && pUserName) {
-        pUserName.textContent = nickname;
-    } else {
-        console.error("Le nom d'utilisateur n'est pas défini ou l'élément HTML n'a pas été trouvé.");
-    }
-}
-
-getUserName();
+const pUserNickname = document.querySelector(".container-choice-user__name");
+pUserNickname.textContent = user.nickname;
